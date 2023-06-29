73d63e5f1c0e085212b8acd17abac5ff6bd44172
Wartori54 <toni.rigo.caldentey@gmail.com>
2023-06-29 20:53:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Celeste windows linux-ifier
+
+This repo contains a little script set up a celeste windows install so that it runs natively (with mono) instead of having to use wine or a derivative
+
+# Usefulness
+
+- Modding: modding a windows install doesn't work, but if you make it a linux install, it will
+- Stability: Since it is run *almost* as the devs inteded it, probably its going to give a better experience
+- Performance: it runs as any linux install, instead of being slowed down by windows compatibility layers
+
+This script will be most useful for people stuck on the epic games version of Celeste, since thats the only one which doesn't provide a linux version.
+
+# License
+See LICENSE.md
+
---
+++ celeste_windows_linuxifier.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/sh
+
+CELESTE_LOC=""
+
+read -p "Enter the location of your celeste installation: " CELESTE_LOC
+
+if [ ! -e $CELESTE_LOC/Celeste.exe ]; then
+	echo "Couldn't find Celeste.exe on the location provided, invalid install"
+	exit 1
+fi
+
+# Download kickstart
+echo Downloading files...
+wget https://github.com/flibitijibibo/MonoKickstart/archive/refs/heads/master.zip
+unzip ./master.zip -d .
+
+# Remove innecessary files
+echo Cleaning downloaded files...
+rm -f MonoKickstart-master/kick.bin.aarch64*
+rm -f MonoKickstart-master/kick.bin.osx*
+rm -f MonoKickstart-master/kick.bin.x86_64.debug
+# Throw it into the celeste directory
+echo Copying files...
+cp -rf MonoKickstart-master/precompiled/* $CELESTE_LOC/
+
+# Setup name
+mv $CELESTE_LOC/kick.bin.x86_64 $CELESTE_LOC/Celeste.bin.x86_64
+
+# Finally copy the extra files
+cp -rf extra/* $CELESTE_LOC/
+
+echo Cleaning up...
+rm -rf ./master.zip MonoKickstart-master
+
+echo Done!
+
