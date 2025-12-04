cb6bc99e7fbd7d519335d15a92a109e4292c3238
wartori <wartori54@gmail.com>
2025-12-04 18:10:53 +0100

    Hardcode MonoKickstart commit

--- celeste_windows_linuxifier.sh
+++ celeste_windows_linuxifier.sh
@@ -2,35 +2,38 @@
 
 CELESTE_LOC=""
 
-read -p "Enter the location of your celeste installation: " CELESTE_LOC
+set -e
 
-if [ ! -e $CELESTE_LOC/Celeste.exe ]; then
+read -r -p "Enter the location of your celeste installation: " CELESTE_LOC
+
+if [ ! -e "$CELESTE_LOC/Celeste.exe" ]; then
 	echo "Couldn't find Celeste.exe on the location provided, invalid install"
 	exit 1
 fi
 
 # Download kickstart
 echo Downloading files...
-wget https://github.com/flibitijibibo/MonoKickstart/archive/refs/heads/master.zip
-unzip ./master.zip -d .
+wget https://github.com/flibitijibibo/MonoKickstart/archive/02b1b80e61c1be2341133cb7e17806728dc55ddd.zip -O kickstart.zip
+unzip ./kickstart.zip -d .
+mv MonoKickstart-* MonoKickstart
 
 # Remove unnecessary files
 echo Cleaning downloaded files...
-rm -f MonoKickstart-master/kick.bin.aarch64*
-rm -f MonoKickstart-master/kick.bin.osx*
-rm -f MonoKickstart-master/kick.bin.x86_64.debug
+rm -f MonoKickstart/kick.bin.aarch64*
+rm -f MonoKickstart/kick.bin.osx*
+rm -f MonoKickstart/kick.bin.x86_64.debug
 # Throw it into the celeste directory
 echo Copying files...
-cp -rf MonoKickstart-master/precompiled/* $CELESTE_LOC/
+cp -rf MonoKickstart/precompiled/* "$CELESTE_LOC/"
 
 # Setup name
-mv $CELESTE_LOC/kick.bin.x86_64 $CELESTE_LOC/Celeste.bin.x86_64
+mv "$CELESTE_LOC/kick.bin.x86_64" "$CELESTE_LOC/Celeste.bin.x86_64"
 
 # Finally copy the extra files
-cp -rf extra/* $CELESTE_LOC/
+cp -rf extra/* "$CELESTE_LOC/"
 
 echo Cleaning up...
-rm -rf ./master.zip MonoKickstart-master
+rm -rf ./kickstart.zip MonoKickstart
 
 echo Done!
 
